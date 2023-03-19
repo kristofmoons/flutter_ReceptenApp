9c1889769539c278bd0327ed2f831fe17a644673
Kristof <r0696321@student.thomasmore.be>
2023-03-19 18:03:33 +0100

    release 0.1

---
+++ lib/screen1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Scherm1 extends StatelessWidget {
+  const Scherm1({super.key});
+
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green);
+  }
+}
